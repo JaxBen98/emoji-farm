eb1f7239df6fc805c966a2d8a6ea2b4ef766d8d8
PandaBoi <buddabane5@gmail.com>
2019-02-15 11:01:38 -0500

    HUGE update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 What is emoji-farm, you may be asking. Well, let me educate you.<br>emoji-farm is something that you can import into a javascript project, to add whatever emojis you want to strings.
 
+`emoji-farm` recently received an entire rewrite. I have some new emojis to add so lets wait for that. Also, thanks for 1,000 downloads! <3
+
 *I would add badges but shields.io doesn't want to create anything correctly*
 
 ## How do i install it?
@@ -16,7 +18,7 @@ or, if you are a *pro*
 
 `$ npm i -g @jaxben98/emoji-farm`
 
-## But how do i use this masterpeice?
+## But how do i use this masterpiece?
 
 It's simple.
 
@@ -44,72 +46,10 @@ Some thing to note is that some emojis are only available on certain platforms. 
 
 ### Doesn't work on some platforms
 
-emoji.rofl(): 🤣
-
-emoji.loved(): 🥰
-
-emoji.think(): 🤔
-
-emoji.eyebrow(): 🤨
-
-emoji.annoyed(): 🙄
-
-emoji.zipped(): 🤐
-
-emoji.drool(): 🤤
-
-emoji.reversed(): 🙃
-
-emoji.moneyMouth(): 🤑
-
-emoji.frown(true): 🙁
-
-emoji.exploding(): 🤯
-
-emoji.hot(): 🥵
-
-emoji.cold(): 🥶
-
-emoji.crazy(): 🤪
-
-emoji.angry(swear): 🤬
-
-emoji.sick(): 🤢
-
-emoji.sick(false, true): 🤒
-
-emoji.sick(false, false, true): 🤕
-
-emoji.sick(false, false, false, true): 🤮
-
-emoji.sick(false, false, false, false, true): 🤧
-
-emoji.smile(false, false, false, false, true): 🤠
-
-emoji.smile(false, false, false, false, false, true): 🤡
-
-emoji.party(): 🥳
-
-emoji.dizzy(true): 🥴
-
-emoji.liar(): 🤥
-
-emoji.hush(): 🤫
-
-emoji.smile(false, true): 🧐
-
-emoji.neutral(false, true): 🧐
-
-emoji.nerd(): 🤓
-
-emoji.robot(): 🤖
+None yet
 
 ## FAQ
 
 **Has the program been finished?**
 
 *It's not even close to being done, no*
-
-**Is there a way to get in contact with you?**
-
-*Try my discord, PandaBoi#4204*
--- test/test.js
+++ test/test.js
@@ -1,81 +1,8 @@
 const emoji = require('./../src/index');
 
-var grin = emoji.grin();
-var bigEyeGrin = emoji.grin(true, false);
-
-console.log(grin);
-console.log("grin = " + emoji.grin());
-console.log("grin big eyes = " + emoji.grin(true))
-console.log("grin smile eyes = " + emoji.grin(false, true))
-console.log(bigEyeGrin);
-console.log("grin sweat eyes = " + emoji.grin(false, false, true, false, true));
-console.log("lol = " + emoji.lol());
-console.log("grin squint eyes = " + emoji.grin(false, false, false, true, false));
-console.log("basic smile = " + emoji.smile()); // unicode
-console.log("smile eyes = " + emoji.smile(true));
-console.log("kiss = " + emoji.kiss());
-console.log("loved = " + emoji.loved()); // not for windows
-console.log("rofl = " + emoji.rofl()); // not for windows
-console.log("kiss smile eyes = " + emoji.kiss(true));
-console.log("think = " + emoji.think()); // not for windows
-console.log("eyebrow = " + emoji.eyebrow()); // not for windows
-console.log("neutral = " + emoji.neutral());
-console.log("no mouth = " + emoji.neutral(true));
-console.log("expressionless = " + emoji.expressionless());
-console.log("smirk = " + emoji.smirk());
-console.log("suprised = " + emoji.suprised());
-console.log("zipped = " + emoji.zipped()); // not for windows
-console.log("tired = " + emoji.tired());
-console.log("sleeping = " + emoji.sleeping());
-console.log("tongue = " + emoji.tongue(false, false, false));
-console.log("tongue winking = " + emoji.tongue(true, false));
-console.log("tounge squinting = " + emoji.tongue(false, true));
-console.log("drooling = " + emoji.drool());
-console.log("meh = " + emoji.meh());
-console.log("confused = " + emoji.confused());
-console.log("reversed = " + emoji.reversed()); // not for windows
-console.log("money mouth = " + emoji.moneyMouth()); // not for windows
-console.log("frown = " + emoji.frown());
-console.log("slight frown = " + emoji.frown(true)); // not for windows
-console.log("worried = " + emoji.worried());
-console.log("exploding head = " + emoji.exploding()); // not for windows
-console.log("screaming = " + emoji.screaming());
-console.log("hot = " + emoji.hot()); // not for windows
-console.log("cold = " + emoji.cold()); // not for windows
-console.log("crazy = " + emoji.crazy()); // not for windows
-console.log("dizzy = " + emoji.dizzy());
-console.log("angry = " + emoji.angry());
-console.log("angry swearing = " + emoji.angry(true)); // not for windows
-console.log("sick = " + emoji.sick()); // not for windows
-console.log("sick with mask = " + emoji.sick(true, false, false));
-console.log("sick with thermometer = " + emoji.sick(false, true)); // not for windows
-console.log("sick with bandages = " + emoji.sick(false, false, true)); // not for windows
-console.log("sick while vomiting = " + emoji.sick(false, false, false, true)); // not for windows
-console.log("sneezing = " + emoji.sick(false, false, false, false, true, true)); // not for windows
-console.log("smiling with halo = " + emoji.smile(false, false, false, true));
-console.log("cowboy = " + emoji.smile(false, false, false, false, true)); // not for windows
-console.log("clown = " + emoji.smile(false, false, false, false, false, true)); // not for windows
-console.log("party = " + emoji.party()); // not for windows
-console.log("drunk = " + emoji.dizzy(true)); // not for windows
-console.log("liar = " + emoji.liar()); // not for windows
-console.log("hush = " + emoji.hush()); // not for windows
-console.log("monocle = " + emoji.neutral(false, true)); // not for windows
-console.log("nerd = " + emoji.nerd()); // not for windows
-console.log("devil = " + emoji.devil());
-console.log("angry devil = " + emoji.devil(true));
-console.log("ogre = " + emoji.ogre());
-console.log("goblin = " + emoji.goblin());
-console.log("skull = " + emoji.skull());
-console.log("ghost = " + emoji.ghost());
-console.log("alien = " + emoji.alien(false));
-console.log("alien monster = " + emoji.alien(true));
-console.log("robot = " + emoji.robot());
-console.log("poop = " + emoji.poop());
-console.log("cat = " + emoji.cat());
-console.log("cat with smiling eyes = " + emoji.cat(true));
-console.log("cat with tears of joy = " + emoji.cat(false, true));
-console.log("cat with heart eyes = " + emoji.cat(false, false, true));
-console.log("smirking cat = " + emoji.cat(false, false, false, true));
-console.log("cat with kissing lips = " + emoji.cat(false, false, false, false, true));
-console.log("tired cat = " + emoji.cat(false, false, false, false, false, true));
-console.log("crying cat = " + emoji.cat(false, false, false, false, false, false, true));
+console.log(emoji.grin());
+console.log(emoji.grin(true));
+console.log(emoji.grin(false, true));
+console.log(emoji.grin(false, false, true));
+console.log(emoji.grin(false, false, false, true));
+console.log(emoji.grin(false, false, false, false, true));
